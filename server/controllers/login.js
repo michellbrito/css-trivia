8ab9824d2ca34d0ecd17a0e2abea6ad2d8b65bea
michell brito <michellbp@msn.com>
2024-05-09 04:07:31 -0400

    register + login route

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,5 +1,34 @@
 const router = require('express').Router()
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require("bcrypt")
+const { users } = new PrismaClient()
 
-router.get('/', async (req, res) => {})
+router.post('/', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await users.findUnique({
+            where: {
+                username,
+            },
+        })
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!user || !isValidPassword) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+        req.session.userId = user.id;
+        res.status(200).json({ message: 'Login successful' });
+        await users.update({
+            where: {
+                username,
+            },
+            data: {
+                logged_in_at: new Date(),
+            },
+        })
+    } catch (err) {
+        console.error('Error logging in:', err);
+        res.status(500).json({ error: 'Failed to log in' });
+    }
+});
 
 module.exports = router
